6ad044645ed9e4f7df5666848db980a24388a6dd
dah4k <84741290+dah4k@users.noreply.github.com>
2022-12-31 20:20:12 -0500

    Docker build Arkime 4.0.3 on PhotonOS
    FIXME: Cannot run Arkime Viewer

---
+++ Makefile
@@ -0,0 +1,25 @@
+DOCKER      ?= docker
+PRODUCTS    ?= arkime
+TAG_PREFIX  ?= local
+
+DOCKERFILE  := docker/Dockerfile
+REQUIREMENTS:= docker/Requirements
+_ANSI_NORM  := \033[0m
+_ANSI_CYAN  := \033[36m
+
+help usage:
+	@grep -hE '^[0-9a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) \
+		| awk 'BEGIN {FS = ":.*?##"}; {printf "$(_ANSI_CYAN)%-20s$(_ANSI_NORM) %s\n", $$1, $$2}'
+
+all: $(PRODUCTS) ## Build all container images
+
+-include $(addsuffix .mk,$(PRODUCTS))
+
+clean: ## Prune container images
+	$(DOCKER) image prune --force
+	$(DOCKER) system prune --force
+
+distclean: ## Delete build artifacts
+	-rm -f artifacts/*
+
+.PHONY: help usage clean distclean
---
+++ arkime.mk
@@ -0,0 +1,25 @@
+arkime-dev:       ## Docker base image for Arkime development
+arkime-runtime:   ## Docker base image for Arkime runtime
+arkime-build:     ## Intermediate Docker image for compiling Arkime
+arkime:           ## Docker image for running Arkime
+
+arkime-dev arkime-runtime:
+	$(DOCKER) build . \
+		--build-arg REQUIREMENTS=$(REQUIREMENTS).$@ \
+		--tag $(TAG_PREFIX)/$@ \
+		--file $(DOCKERFILE).$@
+
+arkime-build: arkime-dev
+	$(DOCKER) build . \
+		--build-arg FROM_IMAGE_BASE=$(TAG_PREFIX)/$< \
+		--tag $(TAG_PREFIX)/$@ \
+		--file $(DOCKERFILE).$@
+
+arkime: arkime-build arkime-runtime
+	$(DOCKER) build . \
+		--build-arg FROM_IMAGE_ARTIFACT=$(TAG_PREFIX)/$(firstword $^) \
+		--build-arg FROM_IMAGE_BASE=$(TAG_PREFIX)/$(lastword $^) \
+		--tag $(TAG_PREFIX)/$@ \
+		--file $(DOCKERFILE).$@
+
+.PHONY: arkime-dev arkime-runtime arkime-build arkime
